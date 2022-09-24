cb21027a51f5e0813ff62f1375ecda6dfa1de19b
thana b <thana.b@outlook.jp>
2022-09-24 10:28:23 +0900

    add grid utils

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,17 @@ const backgroundColor = "#eee";
 export default function Home() {
   return (
     <>
-      <main>
-        <div className="hello utils_box--center-children--stretch">
-          <h2 className="card">Hello World</h2>
-          <h2 className="card"> scss test</h2>
+      <main class="utils_layout--grid-auto-flow">
+        {/* <div className="hello utils_box--center-children--stretch">
+          <h2 className="card utils_layout--limit-for-readibility">Hello World</h2>
+          <h2 className="card utils_layout--limit-for-readibility"> scss test</h2>
         </div>
-        {/* <div className="hello utils_box--center-children--fit">
-        <h2 className="card">Hello World</h2>
-        <h2 className="card"> scss test</h2>
+        <div className="hello utils_box--center-children--fit">
+        <h2 className="card utils_layout--limit-for-readibility">Hello World</h2>
+        <h2 className="card utils_layout--limit-for-readibility"> scss test</h2>
       </div> */}
-        <div className="test-read"></div>
-        <div className="test-read utils_layout--limit-for-readibility">
+
+        <div className="card utils_layout--limit-for-readibility">
           <h1>Lorem Ipsum</h1>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -26,6 +26,8 @@ export default function Home() {
             and more recently with desktop publishing software like Aldus
             PageMaker including versions of Lorem Ipsum.
           </p>
+        </div>
+        <div className="card utils_layout--limit-for-readibility">
           <h2>Lorem Ipsum</h2>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -38,18 +40,8 @@ export default function Home() {
             and more recently with desktop publishing software like Aldus
             PageMaker including versions of Lorem Ipsum.
           </p>
-          <h3>Lorem Ipsum</h3>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
+        </div>
+        <div className="card utils_layout--limit-for-readibility">
           <h4>Lorem Ipsum</h4>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -62,6 +54,8 @@ export default function Home() {
             and more recently with desktop publishing software like Aldus
             PageMaker including versions of Lorem Ipsum.
           </p>
+        </div>
+        <div className="card utils_layout--limit-for-readibility">
           <h5>Lorem Ipsum</h5>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -74,6 +68,8 @@ export default function Home() {
             and more recently with desktop publishing software like Aldus
             PageMaker including versions of Lorem Ipsum.
           </p>
+        </div>
+        <div className="card utils_layout--limit-for-readibility">
           <h6>Lorem Ipsum</h6>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -91,16 +87,11 @@ export default function Home() {
       <style jsx>{`
         .test-read {
           width: 100%;
-          background-color: grey;
+          // background-color: grey;
           margin: auto;
-          margin-bottom: 5em;
         }
 
-        .card {
-          background-color: var(--glob-vars_color-primary-400);
-        }
         .hello {
-          background-color: ${backgroundColor};
           padding: 100px;
           text-align: center;
           transition: 100ms ease-in background;
